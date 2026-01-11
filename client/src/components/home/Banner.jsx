f9f91c41994bdcd41b54a5c47ccfeb2db0d36dbe
Adim806 <adim806@gmail.com>
2026-01-11 18:40:53 +0200

    Style: Update UI components with new color scheme and gradients
    - Changed text and background colors in Banner, CallToAction, Feature, Hero, and Testimonial components to a blue theme for better visual consistency.
    - Updated button styles to reflect the new color scheme.
    - Enhanced gradient backgrounds for improved aesthetics across various sections.
    - Adjusted layout and spacing in Hero and Footer components for better alignment and presentation.

--- client/src/components/home/Banner.jsx
+++ client/src/components/home/Banner.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <div className="w-full py-2.5 font-medium text-sm text-green-800 text-center bg-gradient-to-r from-[#ABFF7E] to-[#FDFEFF]">
-            <p><span className="px-3 py-1 rounded-lg text-white bg-green-600 mr-2">New</span>AI Website Builder</p>
+    <div className="w-full py-2.5 font-medium text-sm text-blue-500 text-center bg-gradient-to-r from-white via-blue-400/60 to-white">
+            <p><span className="px-3 py-1 rounded-lg text-white bg-gradient-to-r from-white via-blue-800 to-white mr-2">New</span>AI Website Builder</p>
     </div>
   )
 }
